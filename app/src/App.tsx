8aec97ca0fcfdf07ba64239d9a8cb60443b41370
Rafael Romariz <94732569+IRMZI@users.noreply.github.com>
2024-06-17 13:48:38 -0300

    Adicionando formulario integrando o hook form com o zod
    Atualizando componentização do MUI, handleling, integrando o formulario com o schema do ZOD

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,46 @@
-import './App.css'
-import Form from './Components/Formpage' 
-import { useEffect } from 'react';
-import { Input, Box, TextField } from "@material-ui/core"
+import "./App.css";
+import Form from "./Components/Formpage";
+import { useEffect } from "react";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import { CssBaseline } from "@material-ui/core";
+import { Box } from "@mui/material";
 
 function App() {
+  const defaultTheme = createTheme();
+
   useEffect(() => {
-    document.title = 'Formulário';
-  }, [])
+    document.title = "Formulário";
+  }, []);
   return (
     <>
-    <Form/>
+      <ThemeProvider theme={defaultTheme}>
+        <Container
+          maxWidth="xs"
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+          }}
+        >
+          <CssBaseline />
+          <Box
+            sx={{
+              width: 400, // Define a largura do fundo quadrado
+              bgcolor: "background.paper", // Define a cor de fundo branca
+              boxShadow: 3, // Adiciona uma sombra ao fundo
+              p: 3, // Adiciona padding
+              borderRadius: 2, // Adiciona bordas arredondadas (opcional)
+              mt: 4, // Adiciona margem no topo
+              textAlign: "center",
+            }}
+          >
+            <Form />
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/src/Components/Formpage.tsx
+++ app/src/Components/Formpage.tsx
@@ -1,80 +1,202 @@
 import { useForm, Controller } from "react-hook-form";
+import { Button, TextField, Typography } from "@material-ui/core";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Box, Button, TextField } from "@material-ui/core";
+const schema = z.object({
+  Company: z.string().min(1, "Nome da empresa é obrigatório"),
+  UniqueID: z.string().min(1, "CNPJ é obrigatório"),
+  FantasyName: z.string(),
+  address: z.object({
+    ZipCode: z
+      .string()
+      .min(1, "CEP é obrigatório")
+      .max(9, "Máximo de 9 dígitos"),
+    Street: z.string().min(1, "Rua é obrigatória"),
+    District: z.string().min(1, "Bairro é obrigatório"),
+    Complement: z.string(),
+    City: z.string().min(1, "Cidade é obrigatória"),
+    State: z.string().min(1, "Estado é obrigatória"),
+    Number: z.string().min(1, "Numero é obrigatória"),
+  }),
+});
 
 function Form() {
   const {
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    criteriaMode: "all",
+    mode: "all",
+    resolver: zodResolver(schema),
+  });
 
   const handleFormSubmit = (data) => {
     console.log(data);
   };
+
   return (
     <>
-      <h2>Olá, bem vindo</h2>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <Controller
-            name="Company"
-            control={control}
-            defaultValue=""
-            rules={{ required: "Nome da empresa é obrigatório" }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Nome da empresa"
-                variant="outlined"
-                error={!!errors.company}
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
-          <Controller
-            name="uniqueID"
-            control={control}
-            defaultValue=""
-            rules={{ required: "CNPJ é obrigatório" }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="CNPJ"
-                variant="outlined"
-                error={!!errors.uniqueID}
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
-          <Controller
-            name="fantasyName"
-            control={control}
-            defaultValue=""
-            rules={{ required: "CNPJ é obrigatório" }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="fantasyName"
-                variant="outlined"
-                error={!!errors.uniqueID}
-                fullWidth
-                margin="normal"
-              />
-            )}
-          />
-        </form>
-      </Box>
+      <Typography variant="h6" component="div" gutterBottom>
+        Olá, bem vindo
+      </Typography>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <Controller
+          name="Company"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Nome da empresa"
+              variant="outlined"
+              error={!!errors.company}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="UniqueID"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="CNPJ"
+              variant="outlined"
+              error={!!errors.UniqueID}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="FantasyName"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Nome fantasia"
+              variant="outlined"
+              error={!!errors.FantasyName}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="ZipCode"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="CEP"
+              variant="outlined"
+              error={!!errors.ZipCode}
+              fullWidth
+              margin="normal"
+              inputProps={{ maxLength: 9 }}
+            />
+          )}
+        />
+        <Controller
+          name="Street"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Rua"
+              variant="outlined"
+              error={!!errors.Street}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="District"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Bairro"
+              variant="outlined"
+              error={!!errors.District}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="Complement"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Complemento"
+              variant="outlined"
+              error={!!errors.Complement}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="City"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Cidade"
+              variant="outlined"
+              error={!!errors.City}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="State"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Estado"
+              variant="outlined"
+              error={!!errors.State}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Controller
+          name="Number"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Numero"
+              variant="outlined"
+              error={!!errors.Number}
+              fullWidth
+              margin="normal"
+            />
+          )}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Enviar
+        </Button>
+      </form>
     </>
   );
 }
